e033159df1b1c24e14a1b0c725c8509df141c372
Corey Leath <corey22blue@hotmail.com>
2026-03-02 18:22:41 -0500

    Update prometheus_metrics.py

--- mlops/prometheus_metrics.py
+++ mlops/prometheus_metrics.py
@@ -1,3 +1,4 @@
-from prometheus_client import Counter
+from prometheus_client import Counter, Histogram
 
 request_counter = Counter("api_requests_total", "Total API Requests")
+inference_latency = Histogram("inference_latency_seconds", "Inference Latency")
