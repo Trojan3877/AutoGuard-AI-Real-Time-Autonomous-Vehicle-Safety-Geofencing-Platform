b4cfde636aa6c3c6064dae15905781f860377129
Corey Leath <corey22blue@hotmail.com>
2026-03-05 13:51:30 -0500

    Create ear_metric.py

---
+++ models/ear_metric.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def compute_ear(eye_points):
+    A = np.linalg.norm(eye_points[1] - eye_points[5])
+    B = np.linalg.norm(eye_points[2] - eye_points[4])
+    C = np.linalg.norm(eye_points[0] - eye_points[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
